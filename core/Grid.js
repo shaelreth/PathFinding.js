6dabe08f6624e4b3a05a4fca73ca6bd64d3272d2
shaelreth <34152899+shaelreth@users.noreply.github.com>
2019-10-17 18:16:59 -0500

    Add files via upload

---
+++ core/Grid.js
@@ -0,0 +1,259 @@
+var Node = require('./Node');
+var DiagonalMovement = require('./DiagonalMovement');
+let Vector3 = require('./Vector3')
+
+/**
+ * The Grid class, which serves as the encapsulation of the layout of the nodes.
+ * @constructor
+ * @param {number} width Number of columns of the grid
+ * @param {number} height Number of rows of the grid.
+ * @param {number} nodeRadius - The radius of the node compared to world space
+ * */
+function Grid(width, height, nodeRadius) {
+
+    /**
+     * The number of columns of the grid.
+     * @type number
+     */
+    this.width = width;
+    /**
+     * The number of rows of the grid.
+     * @type number
+     */
+    this.height = height;
+
+    /**
+     * The radius in world space
+     * @type number
+     */
+    this.nodeRadius = nodeRadius;
+
+    /**
+     * The max grid size width
+     * @type number
+     */
+    this.gridSizeX = Math.round(this.width / (this.nodeRadius * 2));
+
+    /**
+     * The max grid size height
+     * @type number
+     */
+    this.gridSizeY = Math.round(this.height / (this.nodeRadius * 2));
+
+    /**
+     * A 2D array of nodes.
+     */
+    this.nodes = this._buildNodes(width, height, nodeRadius);
+}
+
+/**
+ * Build and return the nodes.
+ * @private
+ * @param {number} width
+ * @param {number} height
+ * @param {number} nodeRadius
+ *     the walkable status of the nodes.
+ * @see Grid
+ */
+Grid.prototype._buildNodes = function(width, height, nodeRadius) {
+    let x = 0 - width / 2
+    let z = 0 - height / 2
+    let worldBottomLeft = new Vector3(x, 0 , z)
+
+    let nodeDiameter = nodeRadius * 2
+    var i, j,
+        nodes = new Array(height);
+
+    for (i = 0; i < height; ++i) {
+        nodes[i] = new Array(width);
+        for (j = 0; j < width; ++j) {
+            let x = worldBottomLeft.x + 1 * (i * nodeDiameter + nodeRadius)
+            let z = worldBottomLeft.z + 1 * (j * nodeDiameter + nodeRadius)
+            let worldPosition = new Vector3(x, 0, z)
+            nodes[i][j] = new Node(j, i, true, worldPosition);
+        }
+    }
+
+    return nodes;
+};
+
+
+Grid.prototype.getNodeAt = function(x, y) {
+    return this.nodes[y][x];
+};
+
+
+/**
+ * Determine whether the node at the given position is walkable.
+ * (Also returns false if the position is outside the grid.)
+ * @param {number} x - The x coordinate of the node.
+ * @param {number} y - The y coordinate of the node.
+ * @return {boolean} - The walkability of the node.
+ */
+Grid.prototype.isWalkableAt = function(x, y) {
+    return this.isInside(x, y) && this.nodes[y][x].walkable;
+};
+
+
+/**
+ * Determine whether the position is inside the grid.
+ * XXX: `grid.isInside(x, y)` is wierd to read.
+ * It should be `(x, y) is inside grid`, but I failed to find a better
+ * name for this method.
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean}
+ */
+Grid.prototype.isInside = function(x, y) {
+    return (x >= 0 && x < this.width) && (y >= 0 && y < this.height);
+};
+
+
+/**
+ * Set whether the node on the given position is walkable.
+ * NOTE: throws exception if the coordinate is not inside the grid.
+ * @param {number} x - The x coordinate of the node.
+ * @param {number} y - The y coordinate of the node.
+ * @param {boolean} walkable - Whether the position is walkable.
+ */
+Grid.prototype.setWalkableAt = function(x, y, walkable) {
+    this.nodes[y][x].walkable = walkable;
+};
+
+/**
+ * Gets the node from a world position
+ * @param {Vector3} worldPosition - The x coordinate of the node.
+ */
+Grid.prototype.getNodeFromWorldPosition = function(worldPosition) {
+    let percentX = (worldPosition.x * this.width / 2) / this.width;
+    let percentY = (worldPosition.z * this.height / 2) / this.width;
+    percentX = percentX > 1 ? 1 : (percentX < 0 ? 0 : percentX);
+    percentY = percentY > 1 ? 1 : (percentY < 0 ? 0 : percentY);
+
+    let x = Math.round((this.gridSizeX - 1) * percentX);
+    let y = Math.round((this.gridSizeY - 1) * percentY);
+
+    return this.nodes[y][x];
+};
+
+
+/**
+ * Get the neighbors of the given node.
+ *
+ *     offsets      diagonalOffsets:
+ *  +---+---+---+    +---+---+---+
+ *  |   | 0 |   |    | 0 |   | 1 |
+ *  +---+---+---+    +---+---+---+
+ *  | 3 |   | 1 |    |   |   |   |
+ *  +---+---+---+    +---+---+---+
+ *  |   | 2 |   |    | 3 |   | 2 |
+ *  +---+---+---+    +---+---+---+
+ *
+ *  When allowDiagonal is true, if offsets[i] is valid, then
+ *  diagonalOffsets[i] and
+ *  diagonalOffsets[(i + 1) % 4] is valid.
+ * @param {Node} node
+ * @param {DiagonalMovement} diagonalMovement
+ */
+Grid.prototype.getNeighbors = function(node, diagonalMovement) {
+    var x = node.x,
+        y = node.y,
+        neighbors = [],
+        s0 = false, d0 = false,
+        s1 = false, d1 = false,
+        s2 = false, d2 = false,
+        s3 = false, d3 = false,
+        nodes = this.nodes;
+
+    // ↑
+    if (this.isWalkableAt(x, y - 1)) {
+        neighbors.push(nodes[y - 1][x]);
+        s0 = true;
+    }
+    // →
+    if (this.isWalkableAt(x + 1, y)) {
+        neighbors.push(nodes[y][x + 1]);
+        s1 = true;
+    }
+    // ↓
+    if (this.isWalkableAt(x, y + 1)) {
+        neighbors.push(nodes[y + 1][x]);
+        s2 = true;
+    }
+    // ←
+    if (this.isWalkableAt(x - 1, y)) {
+        neighbors.push(nodes[y][x - 1]);
+        s3 = true;
+    }
+
+    if (diagonalMovement === DiagonalMovement.Never) {
+        return neighbors;
+    }
+
+    if (diagonalMovement === DiagonalMovement.OnlyWhenNoObstacles) {
+        d0 = s3 && s0;
+        d1 = s0 && s1;
+        d2 = s1 && s2;
+        d3 = s2 && s3;
+    } else if (diagonalMovement === DiagonalMovement.IfAtMostOneObstacle) {
+        d0 = s3 || s0;
+        d1 = s0 || s1;
+        d2 = s1 || s2;
+        d3 = s2 || s3;
+    } else if (diagonalMovement === DiagonalMovement.Always) {
+        d0 = true;
+        d1 = true;
+        d2 = true;
+        d3 = true;
+    } else {
+        throw new Error('Incorrect value of diagonalMovement');
+    }
+
+    // ↖
+    if (d0 && this.isWalkableAt(x - 1, y - 1)) {
+        neighbors.push(nodes[y - 1][x - 1]);
+    }
+    // ↗
+    if (d1 && this.isWalkableAt(x + 1, y - 1)) {
+        neighbors.push(nodes[y - 1][x + 1]);
+    }
+    // ↘
+    if (d2 && this.isWalkableAt(x + 1, y + 1)) {
+        neighbors.push(nodes[y + 1][x + 1]);
+    }
+    // ↙
+    if (d3 && this.isWalkableAt(x - 1, y + 1)) {
+        neighbors.push(nodes[y + 1][x - 1]);
+    }
+
+    return neighbors;
+};
+
+
+/**
+ * Get a clone of this grid.
+ * @return {Grid} Cloned grid.
+ */
+Grid.prototype.clone = function() {
+    var i, j,
+
+        width = this.width,
+        height = this.height,
+        thisNodes = this.nodes,
+
+        newGrid = new Grid(width, height),
+        newNodes = new Array(height);
+
+    for (i = 0; i < height; ++i) {
+        newNodes[i] = new Array(width);
+        for (j = 0; j < width; ++j) {
+            newNodes[i][j] = new Node(j, i, thisNodes[i][j].walkable);
+        }
+    }
+
+    newGrid.nodes = newNodes;
+
+    return newGrid;
+};
+
+module.exports = Grid;
---
+++ core/Node.js
@@ -0,0 +1,35 @@
+let Vector3 = require('./Vector3')
+/**
+ * A node in grid. 
+ * This class holds some basic information about a node and custom 
+ * attributes may be added, depending on the algorithms' needs.
+ * @constructor
+ * @param {number} x - The x coordinate of the node on the grid.
+ * @param {number} y - The y coordinate of the node on the grid.
+ * @param {boolean} [walkable] - Whether this node is walkable.
+ * @param {Vector3} worldSpace - the worldspace position this node represents
+ */
+function Node(x, y, walkable, worldSpace) {
+    /**
+     * The x coordinate of the node on the grid.
+     * @type number
+     */
+    this.x = x;
+    /**
+     * The y coordinate of the node on the grid.
+     * @type number
+     */
+    this.y = y;
+    /**
+     * Whether this node can be walked through.
+     * @type boolean
+     */
+    this.walkable = (walkable === undefined ? true : walkable);
+    /**
+     * The world space where that this represents
+     * @type Vector3
+     */
+    this.worldSpace = worldSpace
+}
+
+module.exports = Node;
